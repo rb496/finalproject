f229ce8730728e8a8919cf71dd636ff83629624c
rahul bhattarai <rb496@njit.edu>
2017-05-07 12:27:13 -0400

    changed layout and keyword

--- test2/signup_process.php
+++ test2/signup_process.php
@@ -8,52 +8,54 @@
 <div id="about" class="container-fluid">
   <div class="row">
     <div class="col-sm-8">
-      <h2>Account Information</h2><br>
-      <h4>"Login" with your credential if you have an account. No account then please signup to create a new user account</h4>
-      <br>
+</div>
     </div>
   </div>
 </div>
-      
-
-
+     <h2>   Account Information</h2>
 <div class="container">
-  <h2>User Info</h2>
   <form method="post" action="createaccount.php">
     <div class="form-group">
-      <!-- <label>Email:</label> -->
-      <input type="text" name="email" class="form-control" id="email" placeholder="Enter email">
-    </div>
-    <div class="form-group">
-      <!-- <label>Password:</label> -->
-      <input type="password" name="password" class="form-control" id="email" placeholder="Enter password">
-    </div>
-    <div class="form-group">
-      <!-- <label>First Name:</label> -->
-      <input type="text" name="fname" class="form-control" id="email" placeholder="Enter first name">
-    </div>
-    <div class="form-group">
-      <!-- <label>Last Name:</label> -->
-      <input type="text" name="lname" class="form-control" id="email" placeholder="Enter last name">
-    </div>
-    <div class="form-group">
-      <!-- <label>Phone Number:</label> -->
-      <input type="text" name="phone" class="form-control" id="email" placeholder="Enter phone number">
-    </div>
-    <div class="form-group">
-      <!-- <label>Date of Birth:</label> -->
-      <input type="date" name="birhtday" class="form-control" id="pwd" >
-    </div>
-    <div class="form-group">
-      <!-- <label>Gender:</label> -->
-      <input type="text" name="gender" class="form-control" id="pwd" placeholder="Enter gender">
-    </div>
+<div style="width: 34%;position: left;right: -30%;background: orange;padding: 8px 10px 8px 12px;color: #fff;border-radius: 7px;">
+<form >
+  <fieldset>
+    <legend>Register:</legend>
+    First name:<br>
+    <input type="text" name="firstname" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
+    <br>
+    Last name:<br>
+    <input type="text" name="lastname" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
+    <br>
+    E-mail address:<br>
+    <input type="text" name="email" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
+    <br>
+     Password: <br>
+    <input type="password" name="password" class="form-control" id="email" value=""style="padding: 2px 7px 10px 9px;">
+    <br>
+    Phone no:<br>
+    <input type="text" name="phone" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
+    <br>
+    Birthdate:<br>
+    <input type="date" name="birthday" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
+    <br>
+    Gender:<br>
+    <input type="radio" name="gender" value="male" checked> Male<br>
+   <input type="radio" name="gender" value="female"> Female<br>
+    <br>
     <input type="submit" action="createaccount.php" value="Create Account" class="btn btn-default">
-  </form>
-</div><br>
+  </fieldset>
+</div>
 </body>
 </html>
 
+<html>
+<div class="container">
+  <h4>Have an account?</h4>
+  <form method="post" action="login.php">
+    <input type="submit" value="Login" class="btn btn-default">
+  </form>
+</div>
+</html>
 
 <?php include 'view/footer.php'; ?>
 
