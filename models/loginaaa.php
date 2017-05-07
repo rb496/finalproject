607faa6bd75f34cc0a6e6cfab8862343e95d5d1c
rahul bhattarai <rb496@njit.edu>
2017-05-06 20:05:51 -0400

    added more functions

---
+++ models/loginaaa.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<br><br><br><br>
+<div style="width: 24%;position: relative;right: -40%;background: red;padding: 8px 10px 8px 12px;color: #fff;border-radius: 7px;">
+<form >
+  <fieldset>
+    <legend>Register:</legend><br>
+    First name:<br>
+    <input type="text" name="firstname" value="" style="padding: 2px 7px 10px 9px;">
+    <br><br>
+    Last name:<br>
+    <input type="text" name="firstname" value="" style="padding: 2px 7px 10px 9px;">
+    <br><br>
+    E-mail address:<br>
+    <input type="text" name="firstname" value="" style="padding: 2px 7px 10px 9px;">
+    <br><br>
+    Phone no:<br>
+    <input type="text" name="lastname" value="" style="padding: 2px 7px 10px 9px;">
+    <br><br>
+    Birthday:<br>
+    <input type="date" name="firstname" value="" style="padding: 2px 7px 10px 9px;">
+    <br><br>
+    Gender:<br>
+    <input type="radio" name="gender" value="male" checked> Male<br>
+   <input type="radio" name="gender" value="female"> Female<br>
+    <br>
+    <input type="submit" value="Submit">
+  </fieldset>
+</form>
+</div>
+</body>
+</html>
