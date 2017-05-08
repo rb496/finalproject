b10f959abe6943935cdf28ba19b9aa9a48cf2a2b
rahul bhattarai <rb496@njit.edu>
2017-05-08 11:28:31 -0400

    change fonts and heading

--- finalproject/view/footer.php
+++ finalproject/view/footer.php
@@ -3,7 +3,7 @@
 <body>
 <footer class="container-fluid text-center">
     <p class="copyright" style="color:black;">
-    	<a href="logout.php">Log Out</a><br>
+    <i>	<a href="logout.php">Sign Out</a><br></i>
         &copy; <?php echo date("Y"); ?> Agenda.
     </p>
 </footer>
