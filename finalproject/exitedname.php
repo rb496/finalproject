a8d77368044e6590bf73d9165d5f749ae4b2dd96
rahul bhattarai <rb496@njit.edu>
2017-05-08 11:24:14 -0400

    changed headings

--- finalproject/exitedname.php
+++ finalproject/exitedname.php
@@ -5,8 +5,8 @@
 	<div id="about" class="container-fluid">
 	  <div class="row">
 	    <div class="col-sm-8">
-	      <h4>The email has exited!</h4>
-	      <br><a href="signup_process.php"> Go back to sign up page</a>
+	      <h4>Looks like the email has been logged off, please go back to signup page</h4>
+	     <b> <br><a href="signup_process.php"> Go to sign up page</a></b>
 	      <br>
 	    </div>
 	  </div>
