14274ea02c8437cf6f133e1b1a5ee06a77cdd07c
rahul bhattarai <rb496@njit.edu>
2017-05-08 04:08:39 -0400

    added account name while logging in

--- finalproject/signup_process.php
+++ finalproject/signup_process.php
@@ -7,27 +7,25 @@
 <!-- Container (About Section) -->
 <div id="about" class="container-fluid">
   <div class="row">
-    <div class="col-sm-8">
-</div>
+    <class="col-sm-8">
     </div>
   </div>
-</div>
-   <b><h1>CREATE A NEW ACCOUNT<h1>
+  <b><h1>CREATE A NEW ACCOUNT</b></h1>
   <h4>Its free and always will be.</h4>
-   <div style="position: left;right: -30%; background: grey"<b></h2><br>
+   <div style="position: left;right: -40%;; background: gray"</h2><br>
 
 <div class="container">
   <form method="post" action="createaccount.php">
     <div class="form-group">
-<div style="width: 34%;position: left;right: -30%;background: orange;padding: 8px 10px 8px 12px;color: #fff;border-radius: 7px;">
+<div style="width: 34%;position:center;right: -40%;background: orange;padding: 8px 10px 8px 12px;color: #fff;border-radius: 7px;">
 <form >
   <fieldset>
     <legend>Register:</legend>
-    First name:<br>
-    <input type="text" name="firstname" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
+    Fname:<br>
+    <input type="text" name="fname" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
     <br>
-    Last name:<br>
-    <input type="text" name="lastname" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
+    Lname:<br>
+    <input type="text" name="lname" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
     <br>
     Email :<br>
     <input type="text" name="email" class="form-control" id="email" value="" style="padding: 2px 7px 10px 9px;">
