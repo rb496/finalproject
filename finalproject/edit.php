7936f33377a4b135585b034f4470df7a59e89468
rahul bhattarai <rb496@njit.edu>
2017-05-08 11:20:28 -0400

    change color and layout

--- finalproject/edit.php
+++ finalproject/edit.php
@@ -12,7 +12,7 @@
   <div id="about" class="container-fluid">
     <div class="row">
       <div class="col-sm-8">
-        <h1 style="color:black">Add Product</h1>
+       <center><h1 style="color:black">Add your task to edit</h1></center>
       </div>
     </div>
   </div>
@@ -20,25 +20,21 @@
   <div class="container" style="color:black"> 
     <form method="post" action="index.php" >
       <div class="form-group">
-        <label>Task:</label>
-        <input type="text" name="task" class="form-control" id="task" placeholder="Change Task" />
-      </div>
-      <div class="form-group">
-        <label>Start Date:</label>
-        <input type="date" name="createddate" class="form-control" id="createddate" />
-      </div>
-      <div class="form-group">
+      <div style="width: 54%;position: relative;right: -10%;background: orange ;padding: 10px 12px 10px 14px;color: #fff;border-radius: 9px;">
+     <form >
+     <fieldset>
+     <legend>Edit your task:</legend>   
+        Task:
+        <input type="text" name="task" value="" style="padding: 4px 9px 12px 11px" class="form-control" id="task" placeholder="Change Task" />
+        Start Date:<br>
+        <input type="date" name="createddate" style="padding: 4px 9px 12px 11px" class="form-control" id="createddate" />
         <input type="hidden" name="item_id" class="form-control" id="item_id" value="<?php echo $item_id;?>" />
-      </div>
-      <div class="form-group">
-        <label>Due Date:</label>
-        <input type="date" name="duedate" class="form-control" id="duedate" />
-      </div>
-      <div class="form-group">
+        End date:<br> 
+        <input type="date" name="duedate" style="padding: 4px 9px 12px 11px" class="form-control" id="duedate" />
         <input type="hidden" name="action" class="form-control" value="after_edit">
       </div>
       <div class="form-group">
-        <input type="submit" value="Finish Editing" class="btn btn-default" />
+        <center><input type="submit" value="Editing complete" class="btn btn-default" /></center>
       </div>
     </form>
   </div>
