86bf186ca3c8b42bc9484190eec7ba4b3fd2a65d
rahul bhattarai <rb496@njit.edu>
2017-05-08 00:03:55 -0400

    changed color and frame

--- new2/addtask.php
+++ new2/addtask.php
@@ -6,34 +6,31 @@
   <div id="about" class="container-fluid">
     <div class="row">
       <div class="col-sm-8">
-        <h1 style="color:black">Add Product</h1>
+     <u><center><h1 style="color:Black">New Agenda</h1></center></u>
       </div>
     </div>
   </div>
 
-  <div class="container" style="color:black"> 
+  <div class="container" style="color:orange"> 
     <form method="post" action="index.php" >
       <div class="form-group">
-        <label>Task:</label>
-        <input type="text" name="task" class="form-control" id="task" placeholder="Enter Task">
-      </div>
-      <div class="form-group">
-        <label>Start Date:</label>
-        <input type="date" name="createddate" class="form-control" id="createddate" >
-      </div>
-      <div class="form-group">
-        <label>Due Date:</label>
-        <input type="date" name="duedate" class="form-control" id="duedate">
-      </div>
-      <div class="form-group">
-        <label>Due Date:</label>
+      <div style="width: 54%;position: relative;right: -20%;background: orange ;padding: 10px 12px 10px 14px;color: #fff;border-radius: 9px;">
+<form >
+<fieldset>
+    <legend>Create new task:</legend>
+    Task:<br>
+        <input type="text" name="task" value="" style="padding: 4px 9px 12px 11px" class="form-control" id="task" placeholder="Enter task">
+        start date:<br>
+        <input type="date" name="createddate"  value="" style="padding: 4px 9px 12px 11px" class="form-control" id="createddate" >
+        end date:<br>
+        <input type="date" name="duedate"  value="" style="padding: 4px 9px 12px 11px" class="form-control" id="duedate">
         <input type="hidden" name="action" class="form-control" value="add_task">
-      </div>
-      <div class="form-group">
-        <input type="submit" value="Add New Task" class="btn btn-default" />
-      </div>
-    </form>
-  </div>
+        </div>
+    <div class="form-group">
+   <b><center><input type="submit" value="Add New Task" style="color:grey" class="btn btn-default" /></center></b>
+    </div>
+   </form>
+</div> 
 
 </body>
 </html>
